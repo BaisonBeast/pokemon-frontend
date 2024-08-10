11f7fff8eeffed6b0e4b3d2d5be6b54dbde025d9
Priyanshu suman <priyanshu@192.168.1.4>
2024-08-10 23:26:52 +0530

    some chagnes

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'; 
+import { RouterProvider } from '@tanstack/react-router';
+import { router } from './router/router.tsx';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+        <RouterProvider router={router} />
       <ToastContainer />
     </QueryClientProvider>
   </React.StrictMode>,
